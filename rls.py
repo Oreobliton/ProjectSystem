323eb0c04102d0df460d7c2afd08e7def875e181
Zorrino <41651765+Zorrino@users.noreply.github.com>
2019-04-18 23:01:33 +0200

    Add files via upload

--- rls.py
+++ rls.py
@@ -80,59 +80,74 @@ def load_options() :
     
 def local_ls() :
     (read, write) = os.pipe()
+
     pid = os.fork()
-    if pid == 0:
-        os.dup2(write, 1)
-        os.close(2)
+    
+    if pid == 0 :
+        err = os.open('/dev/null',os.O_WRONLY)
+        os.dup2(err,2)
+        os.close(read)
+        os.dup2(write,1)
+        os.close(write)
+        os.close(err)
         os.execv("/bin/sh", ["sh", "-c", "ls {}".format(FILENAME)])
     else :
-            os.dup2(read, 0)
-            os.close(write)
-            tout_bits = bytes('', encoding= 'utf-8')
-            while True :
-                buff = os.read(0, 10)
-                if len(buff) == 0 : break
-                tout_bits +=buff
-            L = [ x for x in tout_bits.decode().split('\n') if x != '']
-            return L
-global present 
-present = 1
-global ListeFils
+        os.close(write)
+        os.dup2(read,0)
+        buff = os.read(read, 1)
+        while (len(buff) > 0):
+            os.write(1,buff)
+            buff = os.read(read,1)
+        os.close(read)
+        L = [x for x in buff.decode().split('\n') if x != '']
+        os.wait()
+        return L
 ListeFils = []
 
-def handler(arg1, arg2):
-    global present
-    print("RECU LA MIF")
-    print(ListeFils)
-    present = 0
-    for i in ListeFils:
-        print("SIGNAL ENVOYE AUX FILS")
+def handler(arg1,arg2) :
+    for i in ListeFils :
         os.kill(i, signal.SIGUSR1)
-    
+    for j in ListeFils : 
+        os.wait()
+        sys_exit(2)
+
+def handler2(signal,frame) :
+    for i in ListeFils :
+        os.kill(i, signal.SIGUSR1)
+    for j in ListeFils : 
+        os.wait()
+    sys_exit(0)
+
+
 def explorer(dirname,relative_path) :
     """explorateur"""
-    global present
     change_dir(dirname)
+    present = 0
+
     for x in local_ls() :
         print(os.path.join(relative_path, x))
-        if FIRST:
-            os.kill(os.getppid(),signal.SIGUSR1)
-            print("SIGNAL ENVOYER")
+        if FIRST :
+            os.kill(os.getppid(),signal.SIGUSR2)
             sys_exit(0)
+        present = 1
+    Statut = list()
     for subdir in subdirs() :
         pid = os.fork()
-        if pid == 0:
-            ListeFils.append(os.getpid())
-            signal.signal(signal.SIGUSR1, handler)
-            print("la c'est la liste {}".format(ListeFils))
+        ListeFils.append(pid)
+        if pid == 0 :
+            ListeFils.clear()
             explorer(subdir, os.path.join(relative_path, subdir))
-    while present:
-        os.waitpid(-1, 0)
-    present = 0
-    
+        else :
+            Statut.append(os.wait()[1])
+    Tout_processes = 0
+    if all(Statut):
+        Tout_processes = 2
+    sys_exit(Tout_processes)
 
 def main() :
     """fonction principale"""
+    signal.signal(signal.SIGUSR1,handler)
+    signal.signal(signal.SIGUSR2,handler2)
     load_options()
     explorer('.','')
  
