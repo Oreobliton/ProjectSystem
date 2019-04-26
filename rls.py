14c746b6218beac75010ee1b5205da122d691a34
Zorrino <41651765+Zorrino@users.noreply.github.com>
2019-04-26 08:40:46 +0200

    Add files via upload
    VERSION FINALE DU CUL

--- rls.py
+++ rls.py
@@ -8,16 +8,16 @@ import sys, os, time, random, argparse, signal, socket, select
 
 ### ETUDIANT NUMERO 1
 
-# NOM :
-# PRENOM :
-# NUM ETUDIANT
+# NOM : Kouyoumdjian
+# PRENOM : Pierre
+# NUM ETUDIANT : 21705950
 
 
 ### ETUDIANT NUMERO 2
 
-# NOM
-# PRENOM
-# NUM ETUDIANT
+# NOM : Picard
+# PRENOM : Tristan
+# NUM ETUDIANT : 21704153
 
 
 
@@ -79,73 +79,68 @@ def load_options() :
 ####### FONCTIONS A MODIFIER  #####################################
     
 def local_ls() :
-    (read, write) = os.pipe()
+    (read, write) = os.pipe() #On créer un pipe pour remplacer la sortie et l'entrée standard
 
-    pid = os.fork()
+    pid = os.fork() #On fork pour pourvoir lancer un execv en arrière plan 
     
     if pid == 0 :
-        err = os.open('/dev/null',os.O_WRONLY)
-        os.dup2(err,2)
-        os.close(read)
-        os.dup2(write,1)
-        os.close(write)
+        err = os.open('/dev/null',os.O_WRONLY) 
+        os.dup2(err,2)   #On redirige la sortie erreur vers /dev/null
+        os.close(read)  #On écrit donc on ferme read
+        os.dup2(write,1)  #On remplace la sortie standard par le write 
+        os.close(write) 
         os.close(err)
-        os.execv("/bin/sh", ["sh", "-c", "ls {}".format(FILENAME)])
+        os.execv("/bin/sh", ["sh", "-c", "ls {}".format(FILENAME)]) #On éxécute sh -c ls  dans un sous terminal
     else :
-        os.close(write)
-        os.dup2(read,0)
-        buff = os.read(read, 1)
-        while (len(buff)>0):
-            os.write(1, buff)
-            buff = os.read(read,1)
-        os.close(read)
-        L = [x for x in buff.decode().split('\n') if x != '']
+        os.close(write) 
         os.wait()
+        buff = os.read(read, 10) #On stock le début de la sortie du pipe dans une variable
+        while (len(buff)>0): #On stock jusqu'à ce que le pipe soit vide
+            os.write(1,buff)
+            buff = os.read(read,10)
+        os.close(read)
+        L = [x for x in buff.decode().split('\n') if x != ''] #On remet tout en str (car dans le pipe c'est en bits, et on sépare
         return L
-ListeFils = []
+    
+ListeFils = [] #Liste global pour stocker le pid des fils 
 
-def handler(arg1,arg2) :
-    for i in ListeFils :
+def handler(arg1,arg2) : #Fonction en réception d'un signal quand ce n'est pas la racine
+    for i in ListeFils : #On parcourt ListeFils et on os.kill pour chaque 
         os.kill(i, signal.SIGUSR1)
-    for j in ListeFils : 
+    for j in ListeFils : #On attend chaque fils puis on arrête le processus
         os.wait()
         sys_exit(2)
 
-def handler2(signal,frame) :
+def handler2(signal,frame) : #Pareil que le handler mais seulement pour le processus racine
     for i in ListeFils :
         os.kill(i, signal.SIGUSR1)
     for j in ListeFils : 
         os.wait()
-    sys_exit(0)
-
+    sys_exit(2)
 
 def explorer(dirname,relative_path) :
     """explorateur"""
     change_dir(dirname)
-    present = 0
 
-    for x in local_ls() :
+    for x in local_ls() : #Quand on a trouvé le fichier on print son chemin
         print(os.path.join(relative_path, x))
-        if FIRST :
+        if FIRST : #Si first_match est activé alors on envoi un signal au processus racine et on arrête ce processus
             os.kill(os.getppid(),signal.SIGUSR2)
             sys_exit(0)
-        present = 1
-    Statut = list()
     for subdir in subdirs() :
-        pid = os.fork()
-        ListeFils.append(pid)
+        pid = os.fork() #Pour chaque dossier on créer un processus qui l'explorera
+        ListeFils.append(pid) #On ajoute son pid à ListeFils
         if pid == 0 :
-            ListeFils.clear()
-            explorer(subdir, os.path.join(relative_path, subdir))
-        else :
-            Statut.append(os.wait()[1])
-    Tout_processes = 0
-    if all(Statut):
-        Tout_processes = 2
-    sys_exit(Tout_processes)
-
-    
-########################################################################## Partie Serveur 
+            explorer(subdir, os.path.join(relative_path, subdir)) #Le fils explore un sous fichier
+    statut_processes = 0 #On stock le statut du processus fils
+    for subdir in subdirs():
+        (pid, statut) = os.waitpid(-1, 0)
+        if os.WIFEXITED(statut):
+            statut_processes = os.WEXITSTATUS(statut)
+        else:
+            statut_processes = os.WEXITSTATUS(statut)
+    sys_exit(statut_processes) #On fini avec le code récupérer dans la boucle juste au dessus
+######################################################################### Partie Serveur 
 def launchServer():
     global FILENAME
     host, port = '', 50000
@@ -177,7 +172,7 @@ def launchServer():
                         os.close(r)
                         os.dup2(w,1)            #La on échange la sortie standard avec l'écriture dans le pipe
                         explorer('.','')        #On appelle, on lance donc la recherche du fichier 
-                        os.close(1)             #On en a plus besoin, donc on ferme l'entrée standard
+                        os.close(1)             #On en a plus besoin, donc on ferme la sortie standard
                         sys.exit(0)             #Quand un fils termine son travail il peut exit()
                     else:                       #Le pere doit juste lire, on peut close w
                         os.close(w)
@@ -193,10 +188,10 @@ def launchServer():
 
 def main() :
     """fonction principale"""
-    signal.signal(signal.SIGUSR1,handler)
+    signal.signal(signal.SIGUSR1,handler) #On attend les signals (les fils héritent de ces signal.signal
     signal.signal(signal.SIGUSR2,handler2)
     load_options()
-    if(SERVER):
+    if(SERVER): #SI -server est en argument alors on lance le serveur sinon on lance direct l'exploreur 
         launchServer()
     else:
         explorer('.','')
